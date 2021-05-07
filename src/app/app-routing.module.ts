e8a413069921b7a278777b6e5a6df65a04ec51c7
Pablo <pablovramos.007@gmail.com>
2021-05-07 19:15:51 +0200

    Pagina Tasks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'tasks',
+    loadChildren: () => import('./tasks/tasks.module').then( m => m.TasksPageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
---
+++ src/app/tasks/tasks.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: 'tasks.page.html',
+  styleUrls: ['tasks.page.css'],
+})
+export class TasksPage {
+
+  constructor() {}
+
+}
